4faf42e9cd85cc08ea24b4763c60da961c8fa0c8
Jimenez Sanchez Salvador <salvador.jimenezs@pemex.com>
2025-04-19 06:32:02 -0600

    commit revisión editor web

--- __manifest__.py
+++ __manifest__.py
@@ -13,26 +13,29 @@
         #'views/assets.xml',
         'views/leaflet_mapa_standalone.xml',
         'views/region_views.xml',
-        'views/snippets/leaflet_mapa_block.xml',
+        #'views/snippets/leaflet_mapa_block.xml',
         'views/website_snippets.xml',
         
     ],
     'assets': {
         'web.assets_frontend': [
-            'web_leaflet_mapa_regiones/static/src/js/leaflet_mapa.js',
+            #'web_leaflet_mapa_regiones/static/src/js/leaflet_mapa.js',
+            '/web_leaflet_mapa_regiones/static/src/js/snippets.js',
             'web_leaflet_mapa_regiones/static/src/css/leaflet_mapa.css',
             'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
             'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-            "web_leaflet_mapa_regiones/static/src/js/leaflet_init.js",
+            #"web_leaflet_mapa_regiones/static/src/js/leaflet_init.js",
         ],
         'web_editor.assets_wysiwyg': [
             # El JS de snippets para el editor
-            '/web_leaflet_mapa_regiones/static/src/js/snippets.js',
+            #'/web_leaflet_mapa_regiones/static/src/js/snippets.js',
+            #'web_leaflet_mapa_regiones/static/src/css/leaflet_mapa.css',
         ],
         'web.assets_backend': [
             # Si necesitas Leaflet en el backend
-            'web_leaflet_mapa_regiones/static/src/js/leaflet_mapa.js',
-            'web_leaflet_mapa_regiones/static/src/css/leaflet_mapa.css',
+            #'web_leaflet_mapa_regiones/static/src/js/leaflet_mapa.js',
+            #'/web_leaflet_mapa_regiones/static/src/js/snippets.js',
+            #'web_leaflet_mapa_regiones/static/src/css/leaflet_mapa.css',
         ],
     },
     'installable': True,
